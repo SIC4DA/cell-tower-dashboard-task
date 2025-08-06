2eb065e94f81b7840945d4f2569312f8265caa87
Youssef Khaled <youssefkhaled3301@gmail.com>
2025-08-07 01:52:17 +0300

    feat: create Home page component for Cell Tower Dashboard

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./home.module.scss";
+
+export default function HomePage() {
+  return (
+    <div className={styles.container}>
+      <div>Cell Tower Dashboard Task</div>
+    </div>
+  );
+}
