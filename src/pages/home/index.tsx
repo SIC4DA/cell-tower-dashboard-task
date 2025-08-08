fca4e3315ef3f3bfba9864c448132be6e7faad5a
Youssef Khaled <youssefkhaled3301@gmail.com>
2025-08-08 16:21:18 +0300

    feat: enhance HomePage with loading and error handling using HomeLoading and RequestError components

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,24 @@
-import Header from "@/components/header/Header";
 import useGetCellTowers from "@/hooks/cell-towers/useGetCellTowers";
 import Analytics from "./components/Analytics";
 import Charts from "./components/Charts";
 import CellTowersTable from "./components/CellTowersTable";
+import HomeLoading from "./loading";
+import RequestError from "@/components/shared/RequestError";
 
 export default function HomePage() {
-  const { data, loading } = useGetCellTowers();
+  const { data, loading, error } = useGetCellTowers();
 
-  if (loading) return <div>Loading...</div>;
+  // using something like react-query will prevent the need for this condition since we can use the useSuspenseQuery hook,
+  // which will enable us to handle the loading state in a wrapper react suspense component
+  if (loading) return <HomeLoading />;
 
-  if (!data) return <div>No data</div>;
+  if (!data || error) return <RequestError message={error} />;
 
   return (
-    <main>
-      <Header />
+    <>
       <Analytics towers={data} />
       <Charts towers={data} />
       <CellTowersTable data={data} />
-    </main>
+    </>
   );
 }
