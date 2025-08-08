e868e3eaccf6be4106d97aeb4e373fc29352a365
Youssef Khaled <youssefkhaled3301@gmail.com>
2025-08-08 04:05:27 +0300

    feat: add CellTowersTable component with styling and filter options

---
+++ src/pages/home/components/CellTowersTable.tsx
@@ -0,0 +1,41 @@
+import DataTable, { type Column } from "@/components/data-table/DataTable";
+import type { CellTower } from "@/types/tower";
+import styles from "./CellTowersTable.module.scss";
+
+const columns: Column<CellTower>[] = [
+  { key: "id", label: "ID" },
+  { key: "name", label: "Name" },
+  { key: "status", label: "Status" },
+  { key: "networkType", label: "Network Type" },
+  { key: "city", label: "City" },
+  { key: "signalStrength", label: "Signal Strength" },
+];
+
+const filterOptions: {
+  key: keyof CellTower;
+  label: string;
+  options: string[];
+}[] = [
+  { key: "status", label: "Status", options: ["Active", "Offline"] },
+  {
+    key: "networkType",
+    label: "Network Type",
+    options: ["4G", "5G"],
+  },
+  {
+    key: "city",
+    label: "City",
+    options: ["Cairo", "Luxor", "Hurghada", "Alexandria"],
+  },
+];
+
+const CellTowersTable = ({ data }: { data: CellTower[] }) => {
+  return (
+    <section className={styles.cellTowersTableContainer}>
+      <h2>Cell Towers</h2>
+      <DataTable data={data} columns={columns} filterOptions={filterOptions} />
+    </section>
+  );
+};
+
+export default CellTowersTable;
